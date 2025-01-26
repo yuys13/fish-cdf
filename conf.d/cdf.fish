1926c207175297985f19d30de923351c8f3b422d
yuys13 <yuys13@users.noreply.github.com>
2025-01-26 15:35:12 +0900

    refactor: use universal variable instead of local max

--- conf.d/cdf.fish
+++ conf.d/cdf.fish
@@ -11,13 +11,11 @@ function __cdf_chpwd_recent_dirs --on-variable PWD --description 'Recording rece
     set -U __cdf_recent_dirs (string match -v (pwd) $__cdf_recent_dirs)
     set -U --prepend __cdf_recent_dirs (pwd)
 
-    set -l max 500
-    if set -q cdf_recent_dirs_max
-        set max $cdf_recent_dirs_max
-    end
+    set -q cdf_recent_dirs_max
+    or set -U cdf_recent_dirs_max 500
 
-    if test (count $__cdf_recent_dirs) -gt $max
-        set -U __cdf_recent_dirs $__cdf_recent_dirs[..$max]
+    if test (count $__cdf_recent_dirs) -gt $cdf_recent_dirs_max
+        set -U __cdf_recent_dirs $__cdf_recent_dirs[..$cdf_recent_dirs_max]
     end
 end
 
