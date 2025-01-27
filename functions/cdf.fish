467f549ef6f8b1d8d18a429639f87870b23eef62
yuys13 <yuys13@users.noreply.github.com>
2025-01-27 18:35:34 +0900

    Make the internal function an autoload function

--- functions/cdf.fish
+++ functions/cdf.fish
@@ -1,14 +1,3 @@
-function __cdf_clean_chpwd_recent_dirs --description 'Filter non-existent directories from __cdf_recent_dirs'
-    set -l filterd
-
-    for item in $__cdf_recent_dirs
-        if test -d $item
-            set -a filterd $item
-        end
-    end
-
-    set __cdf_recent_dirs $filterd
-end
 function cdf --description 'cd to recent dir'
     __cdf_clean_chpwd_recent_dirs
 
